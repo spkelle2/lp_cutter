3730c94266db427fb6501fd6cc4846a15134ca09
Sean Kelley <seanpkelley94@gmail.com>
2020-11-17 18:29:35 -0500

    refactored to test

--- min_bisection.py
+++ min_bisection.py
@@ -5,17 +5,17 @@ import sys
 import time
 
 
-def solve(n, p, q, cut_proportion):
-    """
+def create_adjacency_matrix(n, p, q):
+    """Generate our graph. The density of each cluster is controlled by p
+    and the density of edges connecting the clusters is controlled by q.
 
-    :param n: size of our adjacency matrix (n x n)
-    :param p: likelihood of edge within cluster
-    :param q: likelihood of edge between clusters
-    :param cut_proportion: what proportion of total constraints to select from
-    those violated to add to our model
-    :return:
+    :param n: Number of vertices to generate in this graph
+    :param p: The likelihood that a given node shares an edge with another given
+    node within its cluster
+    :param q: The likelihood that a given node shares an edge with a given
+    node in another cluster.
+    :return a: 2-D array where a[i,j]=1 if edge exists between i and j, else 0
     """
-
     indices = range(n)
 
     # create our adjacency matrix
@@ -32,68 +32,138 @@ def solve(n, p, q, cut_proportion):
                 a[i, j] = int(q > np.random.uniform())
     # then copy them to bottom left
     a = a + a.transpose()
+    return a
 
-    # create dictionary of triangle inequality constraints not yet added to model
-    # {(index_triple, constraint_number): amount_violated}
+
+def create_constraint_indices(indices):
+    """create dictionary of triangle inequality constraints to be added to the model
+    {(index_triple, constraint_number): amount_violated}
+
+    :param indices: a range(n) representing the numbers to iterate over
+    :return c: a dictionary keyed by index_triple and constraint_number with value
+    later to be set as how violated the index triple's constraint is for the given
+    constraint number
+    """
+
     tri1 = {((i, j, k), 1): 0 for i in indices for j in indices[i + 1:] for k in
             indices if i != k != j}
     tri2 = {((i, j, k), 2): 0 for i in indices for j in indices[i + 1:] for k in
             indices[j + 1:]}
     c = {**tri1, **tri2}
-    cut_size = int(cut_proportion*len(c))
+    return c
 
-    # build our model
-    mdl = gu.Model("min bisection")
-    mdl.setParam(gu.GRB.Param.Method, 1)
 
-    # variables
-    x = {(i, j): mdl.addVar(ub=1, name=f'{i}_{j}') for i in indices
-         for j in indices if i != j}
+class MinBisect:
 
-    # objective
-    mdl.setObjective(gu.quicksum(a[i, j] * x[i, j] for (i, j) in x if i < j),
-                     sense=gu.GRB.MINIMIZE)
+    def __init__(self, n, p, q, cut_proportion):
+        """Create our adjacency matrix and constraint indexes and declare all
+        other needed attributes
 
-    # (3) Equal partition constraint
-    mdl.addConstr(gu.quicksum(x[i, j] for (i, j) in x if i < j) == n**2/4,
-                  name='Equal Partitions')
+        :param n: size of our adjacency matrix (n x n)
+        :param p: likelihood of edge within cluster
+        :param q: likelihood of edge between clusters
+        :param cut_proportion: what proportion of total constraints to select from
+        those violated to add to our model
+        :return:
+        """
+        self.n = n
+        self.indices = range(n)
+        self.a = create_adjacency_matrix(n, p, q)
+        self.c = create_constraint_indices(self.indices)
+        self.cut_size = int(cut_proportion*len(self.c))
+        self.mdl = None
+        self.x = None
 
-    def add_triangle_inequality(i, j, k, t):
+    def instantiate_model(self):
+        """Does everything that solving iteratively and at once will share, namely
+        instantiating the model and variables as well as setting the objective
+        and equal partition constraint.
+
+        :return:
+        """
+        self.mdl = gu.Model("min bisection")
+        self.mdl.setParam(gu.GRB.Param.Method, 1)
+
+        # variables
+        self.x = {(i, j): self.mdl.addVar(ub=1, name=f'{i}_{j}') for i in self.indices
+                  for j in self.indices if i != j}
+
+        # objective
+        self.mdl.setObjective(gu.quicksum(self.a[i, j] * self.x[i, j] for (i, j)
+                                          in self.x if i < j), sense=gu.GRB.MINIMIZE)
+
+        # (3) Equal partition constraint
+        self.mdl.addConstr(gu.quicksum(self.x[i, j] for (i, j) in self.x if i < j)
+                           == self.n ** 2 / 4, name='Equal Partitions')
+
+    def add_triangle_inequality(self, i, j, k, t):
+        """Adds a triangle inequality to the model and removes its index from
+        future candidate constraints.
+
+        :param i: ith index
+        :param j: jth index
+        :param k: kth index
+        :param t: whether this is constraint type 1 or 2
+        :return:
+        """
         if t == 1:
             # (1) 1st triangle inequality constraint
-            mdl.addConstr(x[i, j] <= x[i, k] + x[j, k], name=f'{i}_{j}_{k}_tri1')
+            self.mdl.addConstr(self.x[i, j] <= self.x[i, k] + self.x[j, k],
+                               name=f'{i}_{j}_{k}_tri1')
         else:  # t == 2:
             # (2) 2nd triangle inequality constraint
-            mdl.addConstr(x[i, j] + x[i, k] + x[j, k] <= 2, name=f'{i}_{j}_{k}_tri2')
-        del c[(i, j, k), t]
+            self.mdl.addConstr(self.x[i, j] + self.x[i, k] + self.x[j, k] <= 2,
+                               name=f'{i}_{j}_{k}_tri2')
+        del self.c[(i, j, k), t]
 
-    # Add randomly 100 of the triangle inequality constraints
-    for ((i, j, k), t) in random.sample(c.keys(), min(100, len(c))):
-        add_triangle_inequality(i, j, k, t)
+    def solve_once(self):
+        """Solves the model with all constraints added at once
 
-    mdl.optimize()
-    assert mdl.status == gu.GRB.OPTIMAL, 'small initial solve should make solution'
+        :return:
+        """
+        self.instantiate_model()
+        for ((i, j, k), t) in self.c.keys():  # may need to make a list first
+            self.add_triangle_inequality(i, j, k, t)
 
-    while True:
-        # find all constraints that are violated. Could tolerance here...
-        # no need to normalize since same size vectors
-        c = {((i, j, k), t): x[i, j].x - x[i, k].x - x[j, k].x if t == 1 else
-             x[i, j].x + x[i, k].x + x[j, k].x - 2 for ((i, j, k), t) in c}
-        inf = [k for k in sorted(c, key=c.get, reverse=True) if c[k] > 0][:cut_size]
-        if not inf:
-            break
+        self.mdl.optimize()
+        assert self.mdl.status == gu.GRB.OPTIMAL, 'small initial solve should make solution'
 
-        for ((i, j, k), t) in inf:
-            add_triangle_inequality(i, j, k, t)
+    def solve_iteratively(self):
+        """Solve the model by feeding in only the top most violated constraints,
+        and repeat until no violated constraints remain
 
-        mdl.optimize()
-        assert mdl.status == gu.GRB.OPTIMAL, f"model ended up as: {mdl.status}"
+        :return:
+        """
 
-    print()
+        self.instantiate_model()
+        # Add randomly 100 of the triangle inequality constraints
+        for ((i, j, k), t) in random.sample(self.c.keys(), min(100, len(self.c))):
+            self.add_triangle_inequality(i, j, k, t)
+
+        self.mdl.optimize()
+        assert self.mdl.status == gu.GRB.OPTIMAL, 'small initial solve should make solution'
+
+        while True:
+            # find how much each constraint is violated
+            # no need to normalize since same size vectors
+            self.c = {((i, j, k), t): self.x[i, j].x - self.x[i, k].x - self.x[j, k].x
+                      if t == 1 else self.x[i, j].x + self.x[i, k].x + self.x[j, k].x - 2
+                      for ((i, j, k), t) in self.c}
+            inf = [k for k in sorted(self.c, key=self.c.get, reverse=True) if
+                   self.c[k] > 0][:self.cut_size]
+            if not inf:
+                break
+
+            for ((i, j, k), t) in inf:
+                self.add_triangle_inequality(i, j, k, t)
+
+            self.mdl.optimize()
+            assert self.mdl.status == gu.GRB.OPTIMAL, f"model ended up as: {self.mdl.status}"
 
 
 if __name__ == '__main__':
     start = time.time()
-    solve(n=int(sys.argv[1]), p=float(sys.argv[2]), q=float(sys.argv[3]),
-          cut_proportion=float(sys.argv[4]))
+    mb = MinBisect(n=int(sys.argv[1]), p=float(sys.argv[2]), q=float(sys.argv[3]),
+                   cut_proportion=float(sys.argv[4]))
+    mb.solve_iteratively()
     print(f'solve time: {time.time() - start} seconds')
---
+++ test_min_bisection.py
@@ -0,0 +1,104 @@
+import inspect
+import numpy as np
+import os
+import shutil
+import unittest
+
+from min_bisection import create_constraint_indices, create_adjacency_matrix, MinBisect
+
+
+class TestCreateAdjacencyMatrix(unittest.TestCase):
+
+    def test_correct_density_within_cluster(self):
+        n, p = 100, .5
+        a = create_adjacency_matrix(n, p, .1)
+
+        # ratio of actual edge count in cluster1 to max possible edge count in cluster1
+        p1 = a[:n//2, :n//2].sum()/((n//2)**2 - n//2)
+        self.assertTrue(.45 <= p1 <= .55,
+                        msg=(f'a large intracluster density should be near p={p}'))
+
+        # ratio of actual edge count in cluster2 to max possible edge count in cluster2
+        p2 = a[n//2:, n//2:].sum()/((n//2)**2 - n//2)
+        self.assertTrue(.45 <= p2 <= .55,
+                        msg=(f'a large intracluster density should be near p={p}'))
+
+    def test_correct_density_between_clusters(self):
+        n, q = 100, .1
+        a = create_adjacency_matrix(n, .5, q)
+
+        # ratio of actual edge count between clusters to max possible edge count between clusters
+        k = a[:n//2, n//2:].sum()/((n//2)**2 - n//2)
+        self.assertTrue(.05 <= k <= .15,
+                        msg=(f'large a intercluster density should be near q={q}'))
+
+    def test_reasonable_values(self):
+        a = create_adjacency_matrix(10, .5, .1)
+        self.assertTrue(set(np.unique(a)) == {0, 1}, 'only values are 0 and 1')
+        self.assertTrue(a.trace() == 0, 'no values should exist on diagonal')
+        self.assertTrue((a == a.T).all(), 'transpose should be equal')
+
+    def test_correct_dimension(self):
+        a = create_adjacency_matrix(10, .5, .1)
+        self.assertTrue(a.shape == (10, 10), 'the dimension should be n by n')
+
+    def test_cluster_sizes_correct(self):
+        # ensure cluster sizes are correct by forcing all edges within a cluster
+        # and none between then counting that the total number in each quadrant correct
+        a = create_adjacency_matrix(10, 1, 0)
+        self.assertTrue(a[:5, :5].sum() == 20)
+        self.assertTrue(a[5:, 5:].sum() == 20)
+        self.assertTrue(a[:5, 5:].sum() == 0)
+        self.assertTrue(a[5:, :5].sum() == 0)
+
+        a = create_adjacency_matrix(11, 1, 0)
+        self.assertTrue(a[:5, :5].sum() == 20)
+        self.assertTrue(a[5:, 5:].sum() == 30)
+        self.assertTrue(a[:5, 5:].sum() == 0)
+        self.assertTrue(a[5:, :5].sum() == 0)
+
+
+class TestCreateConstraintIndices(unittest.TestCase):
+    def test_proper_indices(self):
+        indices = range(10)
+        c = create_constraint_indices(indices)
+        good_idx = set()
+        for i in indices:
+            for j in indices:
+                for k in indices:
+                    if i < j and len({i, j, k}) == len([i, j, k]):
+                        self.assertTrue(((i, j, k), 1) in c,
+                                        f'i={i}, j={j}, k={k}, t=1 belongs in c')
+                        good_idx.add(((i, j, k), 1))
+                    if i < j < k:
+                        self.assertTrue(((i, j, k), 2) in c,
+                                        f'i={i}, j={j}, k={k}, t=1 belongs in c')
+                        good_idx.add(((i, j, k), 2))
+        self.assertFalse(set(c.keys()).difference(good_idx),
+                         'there should not be any more indices')
+
+
+class TestAddTriangleInequality(unittest.TestCase):
+
+    def test_adds_and_deletes(self):
+        mb = MinBisect(8, .5, .1, .1)
+        mb.instantiate_model()
+        ((i, j, k), t) = [k for k in mb.c.keys()][0]
+        mb.add_triangle_inequality(i, j, k, t)
+        self.assertTrue(mb.mdl.getConstrByName(f'{i}_{j}_{k}_tri{t}'),
+                        f'constraint {i}_{j}_{k}_tri{t} should be added')
+        self.assertFalse(((i, j, k), t) in mb.c,
+                         f"(({i}, {j}, {k}), {t}) should be removed from c")
+
+
+class TestInstantiateModel(unittest.TestCase):
+    def everything_that_should_be_is(self):
+        indices = range(8)
+        mb = MinBisect(8, .5, .1, .1)
+        mb.instantiate_model()
+        for i in indices:
+            for j in indices:
+                if i != j:
+                    self.assertTrue("x[i,j] exists")
+        self.assertTrue(mb.mdl.getConstrByName(f'Equal Partitions'),
+                        f'Equal Partition Constraint should exist')
