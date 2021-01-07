79e0b7cbb2c0847adcb9704fb2fb2822ca1b2aba
Sean Kelley <seanpkelley94@gmail.com>
2021-01-06 18:35:52 -0600

    updated runner for min orders

--- runner.py
+++ runner.py
@@ -5,12 +5,12 @@ Call with python runner.py <fldr> <n_1> ... <n_m>
 from itertools import product
 import sys
 
-from min_bisection import MinBisect, solution_schema
+from min_bisection import MinBisect, solution_schema, create_constraint_indices
 
 
 # since they're random, we could maybe have once over solves go on their own
 def run_experiments(ns, ps, qs, cut_proportions=None, numbers_of_cuts=None,
-                    repeats=1, warm_starts=None, methods=None, fldr='run_results'):
+                    min_orders=None, repeats=1, fldr='run_results'):
     """Solve all at once and solve iteratively the min bisection problem
     <repeats> times for each combo of n, p, q, and cut_proportion/number_of_cuts.
 
@@ -19,41 +19,46 @@ def run_experiments(ns, ps, qs, cut_proportions=None, numbers_of_cuts=None,
     :param qs: list of inter-cluster edge likelihoods to try
     :param cut_proportions: list of cut proportions to try for each n, p, q combo
     :param numbers_of_cuts: list of fixed number of cuts to try for each n, p, q combo
+    :param min_orders: list of minimum order of magnitude larger search set must
+    be than desired selected cut set
     :param repeats: how many times to rerun each n, p, q, cut_proportion/fixed cuts combo
-    :param warm_starts: list of booleans representing whether or not to warm start
-    iterative solves
-    :param methods: list of strings of solve methods to try
     :param fldr: where in the working directory to save the results of all
     experiments to csv's
     :return:
     """
     numbers_of_cuts = [] if not numbers_of_cuts else numbers_of_cuts
     cut_proportions = [] if not cut_proportions else cut_proportions
-    warm_starts = [True] if not warm_starts else warm_starts
-    methods = ['dual'] if not methods else methods
+    min_orders = [0] if not min_orders else min_orders
+
+    cuts_possible = {n: len(create_constraint_indices(range(n))) for n in ns}
 
     output = solution_schema.TicDat()
+
+    # don't test combinations with min order that would just rerun using the
+    # whole set of unadded cuts
     combinations = [
-        {'n': n, 'p': p, 'q': q, 'cut_proportion': cut_proportion} for
-        (n, p, q, cut_proportion) in product(ns, ps, qs, cut_proportions)
+        {'n': n, 'p': p, 'q': q, 'cut_proportion': cut_proportion,
+         'min_order': min_order} for (n, p, q, cut_proportion, min_order) in
+        product(ns, ps, qs, cut_proportions, min_orders) if min_order == 0 or
+        10**min_order*cut_proportion*cuts_possible[n] < cuts_possible[n]
     ] + [
-        {'n': n, 'p': p, 'q': q, 'number_of_cuts': number_of_cuts} for
-        (n, p, q, number_of_cuts) in product(ns, ps, qs, numbers_of_cuts)
+        {'n': n, 'p': p, 'q': q, 'number_of_cuts': number_of_cuts,
+         'min_order': min_order} for (n, p, q, number_of_cuts, min_order) in
+        product(ns, ps, qs, numbers_of_cuts, min_orders) if min_order == 0 or
+        10**min_order*number_of_cuts < cuts_possible[n]
     ]
     combinations *= repeats
     combinations.sort(key=lambda x: x['n'])
 
     for i, combo in enumerate(combinations):
-        print(f'running test {i} of {len(combinations)}')
+        print(f'running test {i+1} of {len(combinations)}')
         mb = MinBisect(**combo, solve_id=i, tolerance=0)
-        for method in methods:
-            mb.solve_once(method)
-            for warm_start in warm_starts:
-                mb.solve_iteratively(warm_start, method)
-            for t in solution_schema.all_tables:
-                for pk, f in getattr(mb.data, t).items():
-                    getattr(output, t)[pk] = f
-            solution_schema.csv.write_directory(output, fldr, allow_overwrite=True)
+        mb.solve_once(method='auto')
+        mb.solve_iteratively(warm_start=True, method='dual')
+        for t in solution_schema.all_tables:
+            for pk, f in getattr(mb.data, t).items():
+                getattr(output, t)[pk] = f
+        solution_schema.csv.write_directory(output, fldr, allow_overwrite=True)
 
 
 if __name__ == '__main__':
@@ -61,10 +66,9 @@ if __name__ == '__main__':
         'ns': [int(n) for n in sys.argv[2:]],
         'ps': [.5, .8],
         'qs': [.1, .2],
-        'numbers_of_cuts': [1000],
-        'repeats': 3,
-        'warm_starts': [True, False],
-        'methods': ['dual', 'auto'],
+        'numbers_of_cuts': [10, 30, 100, 300, 1000, 3000, 10000],
+        'min_orders': [0, 1, 2, 3],
+        'repeats': 5,
         'fldr': sys.argv[1]
     }
     run_experiments(**kwargs)
--- test_runner.py
+++ test_runner.py
@@ -2,20 +2,19 @@ from itertools import product
 import shutil
 import unittest
 
-from min_bisection import solution_schema
+from min_bisection import solution_schema, create_constraint_indices
 from runner import run_experiments
 
 
 class TestRunExperiments(unittest.TestCase):
 
-    ns = [10, 20]
-    ps = [.5, .8]
-    qs = [.1, .2]
+    ns = [10]
+    ps = [.5]
+    qs = [.1]
     cut_proportions = [.1]
     numbers_of_cuts = [10]
     repeats = 1
-    warm_starts = [True, False]
-    methods = ['dual', 'auto']
+    min_orders = [0, 1]
     fldr = 'test_output'
 
     def tearDown(self):
@@ -23,32 +22,23 @@ class TestRunExperiments(unittest.TestCase):
 
     def test_saves_exactly_all_run_data(self):
         run_experiments(self.ns, self.ps, self.qs, self.cut_proportions,
-                        self.numbers_of_cuts, self.repeats, self.warm_starts,
-                        self.methods, self.fldr)
+                        self.numbers_of_cuts, self.min_orders, self.repeats,
+                        self.fldr)
         data = solution_schema.csv.create_tic_dat(self.fldr)
 
-        # for each parameter combo we made we should see a certain number of runs recorded
-        for n, p, q, cut_value in product(self.ns, self.ps, self.qs,
-                                          self.cut_proportions + self.numbers_of_cuts):
-            matching_rows = {pk: f for pk, f in data.summary_stats.items() if
-                             f['n'] == n and f['p'] == p and f['q'] == q and
-                             f['cut_value'] == cut_value}
-            # matching row for each combination of repeat, method, and warm start
-            self.assertTrue(len(matching_rows) ==
-                            len(self.methods)*(1+len(self.warm_starts))*self.repeats)
+        # the three valid combos should each run once and iteratively
+        # min_orders = 1 and cut_proportions = .1 is an invalid combo
+        self.assertTrue(len(data.summary_stats) == 6)
+        for (cut_value, min_order) in \
+                product(self.cut_proportions + self.numbers_of_cuts, self.min_orders):
+            matches = {pk: f for pk, f in data.summary_stats.items() if
+                       f['cut_value'] == cut_value and f['min_order'] == min_order}
+            if cut_value == .1 and min_order == 1:
+                self.assertFalse(matches)
+            else:
+                self.assertTrue(len(matches) == 2)
 
-        # every combo recorded should belong to a combo we made
-        wd = {True: 'warm', False: 'cold'}
-        for pk, f in data.summary_stats.items():
-            self.assertTrue(f['n'] in self.ns)
-            self.assertTrue(f['p'] in self.ps)
-            self.assertTrue(f['q'] in self.qs)
-            self.assertTrue(f['cut_value'] in self.cut_proportions or
-                            f['cut_value'] in self.numbers_of_cuts)
-            self.assertTrue(pk[3] in [wd[ws] for ws in self.warm_starts])
-            self.assertTrue(pk[2] in self.methods)
 
-    # both also confirm we work without giving warm starts or methods
     def test_works_with_no_proportions(self):
         run_experiments(self.ns, self.ps, self.qs,
                         numbers_of_cuts=self.numbers_of_cuts,
