e4779c4c8afb242b23828122f9310c1af816030d
Sean Kelley <seanpkelley94@gmail.com>
2021-01-20 10:17:22 -0600

    reorganized to accommodate single graph outputs

--- batch_maker.py
+++ batch_maker.py
@@ -4,7 +4,7 @@ def make_pbs_files(ns, base_name):
         txt = f"""#PBS -N {base_name}_{n}
 #PBS -e /home/sek519/lp_cutter/{base_name}_{n}.err
 #PBS -o /home/sek519/lp_cutter/{base_name}_{n}.out
-#PBS -l nodes=1:ppn=4,mem={4 if n <= 100 else 10}gb,vmem={6 if n <= 100 else 15}gb
+#PBS -l nodes=1:ppn=4,mem={4 if n <= 100 else 8}gb,vmem={6 if n <= 100 else 12}gb
 #PBS -q {'verylong' if n > 150 else 'long' if n > 50 else 'medium'}
 
 cd /home/sek519/lp_cutter
