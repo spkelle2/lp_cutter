a7856b75dedf606342154f7e0a836e7ea5a9148f
Sean Kelley <seanpkelley94@gmail.com>
2021-02-16 12:46:38 -0800

    updated batch_maker

--- batch_maker.py
+++ batch_maker.py
@@ -4,7 +4,7 @@ def make_pbs_files(ns, base_name):
         txt = f"""#PBS -N {base_name}_{n}
 #PBS -e /home/sek519/lp_cutter/{base_name}_{n}.err
 #PBS -o /home/sek519/lp_cutter/{base_name}_{n}.out
-#PBS -l ncpus=4,mem=4gb,vmem=6gb
+#PBS -l ncpus=4,mem=20gb,vmem=30gb
 #PBS -q {'verylong' if n > 150 else 'long' if n > 50 else 'medium'}
 
 cd /home/sek519/lp_cutter
